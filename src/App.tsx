175f3a7b106fa2163f9bde99e5851e27acaa62fd
isakiDev <castillicatrifol@gmail.com>
2023-11-01 22:13:37 -0300

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import products from './mock/products.json'
 
 const initialState = products
 
-
 export function App () {
   const [listState] = useState(initialState)
   const [toggleMenu, setToggleMenu] = useState(false)
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,3 +4,4 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ]
+}
