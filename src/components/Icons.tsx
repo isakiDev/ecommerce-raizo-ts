f8baf7ea06a326db3feb48a49d7c5321e4210123
DSekai <manu.eduardo@live.cl>
2023-11-03 01:35:22 -0300

    push

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -9,3 +9,11 @@ export const ShoppingCart = () => (
 export const Logo = () => (
   <svg color="white" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512"><path fill="currentColor" d="M483.13 32.23a19.65 19.65 0 0 0-2.54-.23h-449C23 31.88 16.12 38.88 16 47.75a11.44 11.44 0 0 0 .23 2.8l65.3 411.25a22.52 22.52 0 0 0 7 12.95A20 20 0 0 0 102 480h313.18a15.45 15.45 0 0 0 15.34-13.42l38.88-247.91H325.19l-18.46 112H205.21l-25.73-148h295.58l20.76-132c1.27-8.75-4.38-17.04-12.69-18.44Z"/></svg>
 )
+
+export const FullScreen = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="118" height="118" viewBox="0 0 20 20"><path fill="#ffffff" d="M2.8 15.8L0 13v7h7l-2.8-2.8l4.34-4.32l-1.42-1.42L2.8 15.8zM17.2 4.2L20 7V0h-7l2.8 2.8l-4.34 4.32l1.42 1.42L17.2 4.2zm-1.4 13L13 20h7v-7l-2.8 2.8l-4.32-4.34l-1.42 1.42l4.33 4.33zM4.2 2.8L7 0H0v7l2.8-2.8l4.32 4.34l1.42-1.42L4.2 2.8z"/></svg>
+)
+
+export const Random = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><g id="IconifyId18b936cdbab60156d0" fill="none" fillRule="evenodd" stroke="none" strokeWidth="1"><g id="IconifyId18b936cdbab60156d1" fill="#ffffff"><path id="IconifyId18b936cdbab60156d2" d="M4 17a1 1 0 0 1 0-2h2l3-3l-3-3H4a1.001 1.001 0 0 1 0-2h3l4 4l4-4h2V5l4 3.001L17 11V9h-1l-3 3l3 3h1v-2l4 3l-4 3v-2h-2l-4-4l-4 4H4Z"/></g></g></svg>
+)
