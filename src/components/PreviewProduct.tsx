9b6a567e8f7cc6340917258a33a6faee81663b04
DSekai <manu.eduardo@live.cl>
2023-11-06 00:41:40 -0300

    viewProduct finished

--- src/components/PreviewProduct.tsx
+++ src/components/PreviewProduct.tsx
@@ -3,17 +3,15 @@ import { FullScreen } from './Icons'
 export const PreviewProduct = () => {
   return (
         <>
-            <div className="flex justify-end my-2 ">
-                <div className="bg-black w-[50px] h-[40px] mb-1 rounded-full flex justify-center items-center text-center text-white">
-                    <span className=" text-ellipsis text-sm">Sold out</span>
-                </div>
-            </div>
-            <div className="flex justify-center items-center">
-                <img className="h-max w-[300px]" src="http://placehold.it/300x300/999/CCC" alt="" />
-            </div>
-            <div className="flex justify-start ">
-                <div className="bg-black m-[5px] w-[25px] h-[20px] rounded-full flex justify-center items-center text-center text-white">
-                    <FullScreen />
+            <div className='flex items-center align-middle justify-center'>
+                <div className="flex justify-center items-center relative w-auto">
+                    <div className="bg-black w-[50px] h-[40px] rounded-full flex justify-center items-center text-center text-white absolute right-0 top-0">
+                        <span className=" text-ellipsis text-sm">Sold out</span>
+                    </div>
+                    <img className="h-max w-[300px]" src="http://placehold.it/300x300/999/CCC" alt="" />
+                    <div className="bg-black w-[30px] h-[30px] rounded-full flex justify-center items-center text-center text-white absolute bottom-0 left-0 hover:scale-125 transition-all">
+                        <FullScreen />
+                    </div>
                 </div>
             </div>
         </>
