9b8a2590aa0ddcb795ce27b35b1994d3d53da19d
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2023-11-14 23:34:15 -0300

    wow

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,7 +1,9 @@
 import { type CategoryType, type ListCategoriesType } from '../types'
 
+const host = import.meta.env.VITE_API
+
 const getCategories = async (): Promise<ListCategoriesType> => {
-  const categoriesFound = await fetch('https://api-ecommerce-raizo.vercel.app/categories')
+  const categoriesFound = await fetch(`${host}/categories`)
 
   if (!categoriesFound.ok) throw new Error('Error in fetch categories')
 
